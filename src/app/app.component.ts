de1cb220f8f3f871491a0f6bdc917fbdab2f4954
tomavic <hbasheer@live.com>
2024-03-25 22:19:19 +0200

    chore: layout 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainFooterComponent } from './layout/main-footer/main-footer.component';
+import { MainNavbarComponent } from './layout/main-navbar/main-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainFooterComponent, MainNavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'blog-angular-tailwind';
