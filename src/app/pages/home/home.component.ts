186d0fcf02eda470ec1f7574ab3f4323363b5533
tomavic <hbasheer@live.com>
2024-03-26 22:29:38 +0200

    feat: hero

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../../layout/hero/hero.component';
+import { ProjectsComponent } from '../../layout/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, ProjectsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
