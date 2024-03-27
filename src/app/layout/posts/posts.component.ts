4b96fb63f513081817e81efb4dc92bde73b39fc9
tomavic <hbasheer@live.com>
2024-03-28 01:13:35 +0200

    feat: add posts section

--- src/app/layout/posts/posts.component.ts
+++ src/app/layout/posts/posts.component.ts
@@ -1,12 +1,51 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
 export class PostsComponent {
+  recentPostsList: Post[] = [
+    {
+      id: '1',
+      title: 'Post 1 title',
+      description: 'lorem ipsum',
+      publishedOn: '17 March 2024'
+    },
+    {
+      id: '2',
+      title: 'Post 2 title',
+      description: 'lorem ipsum',
+      publishedOn: '18 March 2024'
+    },
+    {
+      id: '3',
+      title: 'Post 3 title',
+      description: 'lorem ipsum',
+      publishedOn: '19 March 2024'
+    },
+    {
+      id: '4',
+      title: 'Post 4 title',
+      description: 'lorem ipsum',
+      publishedOn: '16 March 2024'
+    },
+    {
+      id: '5',
+      title: 'Post 5 title',
+      description: 'lorem ipsum',
+      publishedOn: '15 March 2024'
+    }
+  ];
+}
 
+export interface Post {
+  id: string;
+  title: string;
+  description: string;
+  publishedOn: string;
 }
