593ea2dc30b80c9888dca3bd5f4ab3c721546d95
Hatem <hbasheer@live.com>
2024-03-27 23:41:18 +0200

    feat: base

--- src/app/layout/projects/projects.component.ts
+++ src/app/layout/projects/projects.component.ts
@@ -1,10 +1,39 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  projectsList: Project[] = [
+    {
+      id: '1',
+      title: 'project 1',
+      imageSrc: 'assets/project-1.png',
+      description: 'Lorem Ipsum'
+    },
+    {
+      id: '2',
+      title: 'project 2',
+      imageSrc: 'assets/project-2.png',
+      description: 'Lorem Ipsum'
+    },
+    {
+      id: '3',
+      title: 'project 3',
+      imageSrc: 'assets/project-3.png',
+      description: 'Lorem Ipsum'
+    }
+  ];
+}
+
+export interface Project {
+  id: string;
+  title: string;
+  imageSrc: string;
+  description: string;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../layout/hero/hero.component';
 import { ProjectsComponent } from '../../layout/projects/projects.component';
+import { PostsComponent } from '../../layout/posts/posts.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, ProjectsComponent],
+  imports: [HeroComponent, ProjectsComponent, PostsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
